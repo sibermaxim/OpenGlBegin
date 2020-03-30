82ef6c684e8db89e858aa1a2bdc8fe4bd3b5d299
Константинов Максим <konstantinovmax75@gmail.com>
2020-03-30 13:00:35 +0700

    Реализован скелет приложения.

--- app/src/main/java/com/example/openglbegin/MainActivity.java
+++ app/src/main/java/com/example/openglbegin/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.openglbegin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if(!supportES2()){
+            Toast.makeText(this, "OpenGl ES 2.0 is not supported", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderer( new OpenGLRenderer());
+        setContentView(glSurfaceView);
+    }
+
+    private  boolean supportES2(){
+        ActivityManager activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        return (configurationInfo.reqGlEsVersion >= 0x20000);
     }
 }
