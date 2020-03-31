8e7a86b34e376bf3db703bf9683f717961f21c30
Константинов Максим <konstantinovmax75@gmail.com>
2020-03-31 22:22:29 +0700

    Реализован скелет приложения.

--- app/src/main/java/com/example/openglbegin/MainActivity.java
+++ app/src/main/java/com/example/openglbegin/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.openglbegin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,6 +42,13 @@ public class MainActivity extends AppCompatActivity {
         glSurfaceView.setEGLContextClientVersion(2);
         glSurfaceView.setRenderer( new OpenGLRenderer());
         setContentView(glSurfaceView);
+        LinearLayout ll = new LinearLayout(this);
+        Button b = new Button(this);
+        b.setText("hello world");
+        ll.addView(b);
+        ll.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
+        this.addContentView(ll,
+                new ActionBar.LayoutParams(ActionBar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.FILL_PARENT));
     }
 
     private  boolean supportES2(){
